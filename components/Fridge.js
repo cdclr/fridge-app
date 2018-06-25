a6101c4c0d598017ba5ac6c92105548ef1c92b0c
khusain <judicatorkhalid@gmail.com>
2018-06-25 16:29:28 -0400

    debug

--- components/Fridge.js
+++ components/Fridge.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Flatlist, Switch} from 'react-native';
+import { View, Text, FlatList, Switch} from 'react-native';
 import FoodItem from './FoodItem.js';
 
 export default class Fridge extends React.Component {
