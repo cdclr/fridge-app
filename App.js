1c2615276e83f87f61bcb9045880bb86cc193048
khusain <judicatorkhalid@gmail.com>
2018-06-25 17:21:38 -0400

    debug

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import Groceries from './components/Groceries.js';
 import Fridge from './components/Fridge.js';
+import ShoppingList from './components/ShoppingList.js';
 
 export default createBottomTabNavigator({
   Fridge: Fridge,
