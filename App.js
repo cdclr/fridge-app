9d8c2de33e89e70765af84560eeeef19ec29fee0
khusain <judicatorkhalid@gmail.com>
2018-06-25 16:29:14 -0400

    debug

--- App.js
+++ App.js
@@ -2,52 +2,8 @@ import React from 'react';
 import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
-import FoodItem from './components/FoodItem.js';
 import Groceries from './components/Groceries.js';
-
-class Fridge extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fridgeItems: [{
-        key: '0',
-        name: 'Apple',
-        isChecked: false,
-      },
-      {
-        key: '1',
-        name: 'Banana',
-        isChecked: false,
-      },
-      {
-        key: '2',
-        name: 'Cherry',
-        isChecked: false,
-      }],
-    };
-  }
-
-  foodItem({item}) {
-      
-    return (
-      <FoodItem item={item} />
-    );
-  }
-
-
-  render() {
-
-    return (
-      <View>
-        <FlatList
-          data={this.state.fridgeItems}
-          renderItem={this.foodItem}
-        />
-        <Text>Fridge</Text>
-      </View>
-    )
-  }
-}
+import Fridge from './components/Fridge.js';
 
 export default createBottomTabNavigator({
   Fridge: Fridge,
---
+++ components/Fridge.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Text, Flatlist, Switch} from 'react-native';
+import FoodItem from './FoodItem.js';
+
+export default class Fridge extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        fridgeItems: [{
+          key: '0',
+          name: 'Apple',
+          isChecked: false,
+        },
+        {
+          key: '1',
+          name: 'Banana',
+          isChecked: false,
+        },
+        {
+          key: '2',
+          name: 'Cherry',
+          isChecked: false,
+        }],
+      };
+    }
+  
+    foodItem({item}) {
+        
+      return (
+        <FoodItem item={item} />
+      );
+    }
+  
+  
+    render() {
+  
+      return (
+        <View>
+          <FlatList
+            data={this.state.fridgeItems}
+            renderItem={this.foodItem}
+          />
+          <Text>Fridge</Text>
+        </View>
+      )
+    }
+  }
